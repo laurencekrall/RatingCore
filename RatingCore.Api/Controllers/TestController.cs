c293d2c35e5ea62bb2dd3c492d694eb7c31737fe
laurencekrall <laurence_krall@hotmail.com>
2019-08-18 00:01:04 +0100

    low - add basic controller to test api up

---
+++ RatingCore.Api/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Cloud.Vision.V1;
+using Microsoft.AspNetCore.Mvc;
+using RatingCore.Api.DTO;
+using RatingCore.Data.Models;
+using RatingCore.GoogleCP;
+using RatingCore.GoogleCP.Models;
+
+namespace RatingCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
